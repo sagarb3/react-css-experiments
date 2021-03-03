d0b76529745310f3d7dc02606f0beadb2789b59d
Sagar Bhattacharya <sagar13912@gmail.com>
2021-03-03 10:56:47 +0530

    updated bit sample

--- src/components/SampleComponent2/index.js
+++ src/components/SampleComponent2/index.js
@@ -1,11 +1,12 @@
 import { style } from '@bit/sagarb3.css-module.button-styled'
 const SampleComponent2 = (props)=>{
-    const h1Style = style.Input.bold;
-    const btStyle = style.Button.red;
+    const h1Style = `${style.Input.bold}`;
+    const btStyle = `${style.Button.red} ${style.Button.medium}`;
     return (
         <div>
             <h1 className={h1Style}>SampleComponet2</h1>
            <button className={btStyle}>Hi</button> 
+           <button className="red">Second Button</button>
         </div>
     )
 }
